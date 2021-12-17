bf229d068bf03d51af7e548abef3e76f1b04e7bf
KDReiners <kdreiners@gmx.de>
2021-12-17 11:34:25 +0100

    First Version of integrating apple health export

--- healthKitConnector/CoreData/Persistence.swift
+++ healthKitConnector/CoreData/Persistence.swift
@@ -12,7 +12,8 @@ class PersistenceController {
 
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
+        let viewContext = result.cloudContainer.viewContext
+        let shamanContext = result.localContainer.viewContext
         do {
             try viewContext.save()
         } catch {
@@ -24,14 +25,17 @@ class PersistenceController {
         return result
     }()
 
-    let container: NSPersistentCloudKitContainer
+    let cloudContainer: NSPersistentCloudKitContainer
+    let localContainer: NSPersistentContainer
     
     init(inMemory: Bool = false) {
-        container = NSPersistentCloudKitContainer(name: "healthKitConnector")
+        cloudContainer = NSPersistentCloudKitContainer(name: "healthKitConnector")
+        localContainer = NSPersistentContainer(name: "shaman")
         if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+            cloudContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+            localContainer.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        cloudContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -47,7 +51,24 @@ class PersistenceController {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
-        container.viewContext.automaticallyMergesChangesFromParent = true
-        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        localContainer.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+
+                /*
+                Typical reasons for an error here include:
+                * The parent directory does not exist, cannot be created, or disallows writing.
+                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                * The device is out of space.
+                * The store could not be migrated to the current model version.
+                Check the error message to determine what the actual problem was.
+                */
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+            
+        })
+        cloudContainer.viewContext.automaticallyMergesChangesFromParent = true
+        cloudContainer.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
     }
 }
